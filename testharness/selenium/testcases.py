40e41952ce461a4ca66528fb486dbfad2e9263d1
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-11-19 16:07:10 -0500

    [INC-2351] Create Web GUI Test Case Harness; users set the uppercase config for browsers.

---
+++ testharness/selenium/testcases.py
@@ -0,0 +1,128 @@
+""" Selenium Web GUI Test Harness for Python unittest
+
+Use these unittest.TestCase subclasses to run Web GUI tests.
+"""
+
+import logging
+import unittest
+
+from browser_emulators.chrome.grid_session import ChromeBrowserGridSession
+from browser_emulators.firefox.grid_session import FirefoxBrowserGridSession
+
+# log package name.
+log = logging.getLogger('.'.join(__name__.split('.')[:-1]))
+
+
+class NoBrowserFactoryError(NotImplementedError):
+    """ No Browser Factory Error.
+
+    This browser brand has no get_driver() factory class.
+    """
+
+    def __init__(self, browser_brand):
+        super().__init__(
+            'Browser brand "{}" has no Web Driver factory class.'.format(
+                browser_brand))
+
+
+class SeleniumWebGUITestCase(unittest.TestCase):
+    """ Selenium Web GUI Test Case.
+
+    This Test Case provides a QA or Regression testing plan.
+
+    This Selenium client describes a Web GUI interface.
+
+
+    A Selenium Grid service is deployed to emulate browsers already.
+    Each test case method should start a browser session and traverse
+    its GUI to validate its health.
+    """
+
+    # Change these constants in the subclass to set the browser instance.
+    GRID_URL = 'http://127.0.0.1:4444/wd/hub'
+
+    BROWSER_BRAND = 'Chrome'  # Chrome, FireFox, ...
+    PLATFORM = 'ANY'          # "ANY" or Linux, Windows, ...
+    VERSION = None            # Browser version (None means ANY)
+
+    HEADLESS = True
+
+    LOG_VERBOSE = True
+    LOG_TO_FILE = None
+
+    ACCEPT_SSL_CERTS = False
+    HTTP_PROXY_HOST = None
+    HTTP_PROXY_PORT = 80
+
+    @classmethod
+    def _select_browser_brand_factory(cls, browser_brand):
+        """ Select Browser Brand Factory Class.
+
+        :param str browser_brand: match BROWSER_BRAND to its class
+        :returns: BrowserGridSession factory or None when not available
+        """
+
+        browser_factory = None
+
+        browser_brand = browser_brand.upper()
+        if browser_brand == 'CHROME':
+            browser_factory = ChromeBrowserGridSession
+        elif browser_brand == 'EDGE':
+            pass
+        elif browser_brand == 'FIREFOX':
+            browser_factory = FirefoxBrowserGridSession
+        elif browser_brand == 'INTERNETEXPLORER':
+            pass
+        elif browser_brand == 'SAFARI':
+            pass
+
+        return browser_factory
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Create a browser factory to get_driver() objects.
+        Each browser session has the "desired capabilities" from the constants.
+        """
+
+        browser_factory_class = cls._select_browser_brand_factory(cls.BROWSER_BRAND)
+        if browser_factory_class is not None:
+            cls.browser_factory = browser_factory_class(
+                command_executor=cls.GRID_URL,
+                # platform=cls.PLATFORM,
+                # version=cls.VERSION,
+                headless=cls.HEADLESS,
+
+                log_verbose=cls.LOG_VERBOSE,
+                log_to_file=cls.LOG_TO_FILE,
+
+                accept_ssl_certs=cls.ACCEPT_SSL_CERTS,
+                proxy_host=cls.HTTP_PROXY_HOST,
+                proxy_port=cls.HTTP_PROXY_PORT,
+
+                raise_errors=True)
+        else:
+            cls.browser_factory = None
+
+    def setUp(self):
+        """ Set Up.
+
+        Create a new browser session "driver" for the current tests.
+        """
+
+        super().setUp()
+        if self.browser_factory is not None:
+            self.driver = self.browser_factory.get_driver()
+        else:
+            raise NoBrowserFactoryError(self.BROWSER_BRAND)
+
+    def tearDown(self):
+        """ Tear Down.
+
+        Quit browser session and release resources.
+        """
+
+        super().tearDown()
+        if self.driver:
+            self.driver.quit()
+            self.driver = None
---
+++ tests/testharness/selenium/test_testcases.py
@@ -0,0 +1,204 @@
+""" SeleniumWebGUITestCase Class Tests.
+
+The SeleniumWebGUITestCase is a helper class for QA Testing.
+We are testing it here with itself, which is confusing.
+"""
+
+from unittest import mock
+
+from browser_emulators.chrome.grid_session import ChromeBrowserGridSession
+from browser_emulators.firefox.grid_session import FirefoxBrowserGridSession
+
+from testharness.selenium.testcases import (
+    NoBrowserFactoryError,
+    SeleniumWebGUITestCase
+)
+
+
+class DefaultWebGUITests(SeleniumWebGUITestCase):
+    """ Default Web GUI Tests.
+
+    Prove that unit tests run the default Web GUI settings.
+    This is effectively a Chrome Web Driver.
+    """
+
+    # Settings: Defaults
+
+    def setUp(self):
+        expected_desired_capabilities = {
+            'browserName': 'chrome', 'version': '', 'platform': 'ANY',
+            'javascriptEnabled': True, 'acceptSslCerts': False,
+            'goog:chromeOptions': {
+                'prefs': {
+                    'download.default_directory': '/tmp/',
+                    'download.directory_upgrade': True,
+                    'download.prompt_for_download': False,
+                    'safebrowsing.enabled': False,
+                    'safebrowsing.disable_download_protection': True
+                },
+                'extensions': [],
+                'args': ['headless', 'no-sandbox', 'window-size=1200x600']
+            }
+        }
+
+        self.mock_driver = mock.MagicMock()
+        with mock.patch('browser_emulators.chrome.grid_session.webdriver.Remote',
+                        return_value=self.mock_driver) as mock_webdriver_remote:
+            super().setUp()
+
+            mock_webdriver_remote.assert_called_with(
+                command_executor=self.GRID_URL,
+                desired_capabilities=expected_desired_capabilities)
+
+    def tearDown(self):
+        super().tearDown()
+        self.mock_driver.quit.assert_called()
+
+    def test_browser_factory_settings(self):
+        self.assertIsInstance(self.browser_factory, ChromeBrowserGridSession)
+
+        self.assertEqual(self.browser_factory.command_executor, self.GRID_URL)
+
+        self.assertEqual(self.BROWSER_BRAND.upper(), 'CHROME')
+        # self.PLATFORM
+        # self.VERSION
+
+        self.assertTrue(self.browser_factory.headless)
+
+        self.assertEqual(self.browser_factory.log_verbose, self.LOG_VERBOSE)
+        self.assertEqual(self.browser_factory.log_to_file, self.LOG_TO_FILE)
+
+        # network capabilities.
+        self.assertFalse(self.browser_factory.capabilities['acceptSslCerts'])  # self.ACCEPT_SSL_CERTS)
+        if self.HTTP_PROXY_HOST is not None:
+            self.assertEqual(self.browser_factory.capabilities['httpProxy'],
+                             '{}:{}'.format(self.HTTP_PROXY_HOST, self.HTTP_PROXY_PORT))
+
+        # fixed values: always raise WebDriver errors in test cases.
+        self.assertEqual(self.browser_factory.download_directory, '/tmp/')
+        self.assertTrue(self.browser_factory.raise_errors)
+
+# =========================================================================
+
+
+class FirefoxWebGUITests(SeleniumWebGUITestCase):
+    """ FireFox Web GUI Tests.
+
+    Prove that unit tests run the Firefox Web GUI settings.
+    This sets up a remote Firefox Web Driver.
+    """
+
+    # Settings: Firefox
+    BROWSER_BRAND = 'FIREFOX'
+
+    ACCEPT_SSL_CERTS = True
+    HTTP_PROXY_HOST = 'secure.proxy.example.com'
+    HTTP_PROXY_PORT = 443
+
+    def setUp(self):
+        expected_desired_capabilities = {
+            'browserName': 'firefox', 'marionette': True,
+            'platform': 'LINUX', 'javascriptEnabled': True,
+            'acceptInsecureCerts': True, 'acceptSslCerts': True
+        }
+
+        self.mock_driver = mock.MagicMock()
+        with mock.patch('browser_emulators.firefox.grid_session.webdriver.Remote',
+                        return_value=self.mock_driver) as mock_webdriver_remote:
+            super().setUp()
+
+            mock_webdriver_remote.assert_called_with(
+                command_executor=self.GRID_URL,
+                desired_capabilities=expected_desired_capabilities,
+                browser_profile=mock.ANY)
+
+    def tearDown(self):
+        super().tearDown()
+        self.mock_driver.quit.assert_called()
+
+    def test_browser_factory_settings(self):
+        self.assertIsInstance(self.browser_factory, FirefoxBrowserGridSession)
+
+        self.assertEqual(self.browser_factory.command_executor, self.GRID_URL)
+
+        self.assertEqual(self.BROWSER_BRAND.upper(), 'FIREFOX')
+        # self.PLATFORM
+        # self.VERSION
+
+        self.assertTrue(self.browser_factory.headless)
+
+        self.assertEqual(self.browser_factory.log_verbose, self.LOG_VERBOSE)
+        self.assertEqual(self.browser_factory.log_to_file, self.LOG_TO_FILE)
+
+        # network capabilities.
+        self.assertTrue(self.browser_factory.capabilities['acceptSslCerts'])
+        # NOTE: Firefox Profile objects can't be read for settings?!?
+
+        # fixed values: always raise WebDriver errors in test cases.
+        self.assertEqual(self.browser_factory.download_directory, '/tmp/')
+        self.assertTrue(self.browser_factory.raise_errors)
+
+# =========================================================================
+
+
+class NoEdgeWebGUITests(SeleniumWebGUITestCase):
+    """ No MS Edge Browser Web GUI Tests.
+
+    Prove that unit tests raise NoBrowserFactoryError.
+    """
+
+    # Settings: Missing Browser
+    BROWSER_BRAND = 'EDGE'
+
+    def setUp(self):
+        with self.assertRaisesRegex(NoBrowserFactoryError, r'^Browser brand "EDGE"'):
+            super().setUp()
+
+    def tearDown(self):
+        self.assertFalse(hasattr(self, 'driver'))
+
+    def test_no_browser_factory(self):
+        """ NoBrowserFactoryError was raised and the "browser_factory" attribute is None """
+        self.assertIsNone(self.browser_factory)
+
+
+class NoInternetExplorerWebGUITests(SeleniumWebGUITestCase):
+    """ No MS Internet Explorer Browser Web GUI Tests.
+
+    Prove that unit tests raise NoBrowserFactoryError.
+    """
+
+    # Settings: Missing Browser
+    BROWSER_BRAND = 'INTERNETEXPLORER'
+
+    def setUp(self):
+        with self.assertRaisesRegex(NoBrowserFactoryError, r'^Browser brand "INTERNETEXPLORER"'):
+            super().setUp()
+
+    def tearDown(self):
+        self.assertFalse(hasattr(self, 'driver'))
+
+    def test_no_browser_factory(self):
+        """ NoBrowserFactoryError was raised and the "browser_factory" attribute is None """
+        self.assertIsNone(self.browser_factory)
+
+
+class NoSafariWebGUITests(SeleniumWebGUITestCase):
+    """ No Mac Safari Browser Web GUI Tests.
+
+    Prove that unit tests raise NoBrowserFactoryError.
+    """
+
+    # Settings: Missing Browser
+    BROWSER_BRAND = 'SAFARI'
+
+    def setUp(self):
+        with self.assertRaisesRegex(NoBrowserFactoryError, r'^Browser brand "SAFARI"'):
+            super().setUp()
+
+    def tearDown(self):
+        self.assertFalse(hasattr(self, 'driver'))
+
+    def test_no_browser_factory(self):
+        """ NoBrowserFactoryError was raised and the "browser_factory" attribute is None """
+        self.assertIsNone(self.browser_factory)
