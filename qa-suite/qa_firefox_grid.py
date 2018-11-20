fc606756ccfc37ba04302cdeea4630cdff566e50
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-11-19 19:55:26 -0500

    [INC-2351] Create a simple qa-suite/ to show how to use these for QA.

---
+++ qa-suite/qa_firefox_grid.py
@@ -0,0 +1,48 @@
+""" QA Testing: SeleniumWebGUITestCase Class.
+
+The SeleniumWebGUITestCase is a helper class for QA Testing.
+We are testing it here with itself, which is confusing.
+
+Selenium Grid opens a remote browser and traverses the UI.
+There is no visible GUI shown during these QA tests.
+
+The example below demonstrates a Firefox browser session.
+"""
+
+from testharness.selenium.testcases import SeleniumWebGUITestCase
+
+
+class QAFirefoxWebGUI(SeleniumWebGUITestCase):
+    """ QA FireFox Web GUI.
+
+    Prove that QA tests run with the Firefox Web GUI settings.
+    This sets up a remote Firefox Web Driver in a Selenium Grid.
+    """
+
+    GRID_URL = 'http://roattnap04.gcsc.att.com:4444/wd/hub'
+
+    # Settings: Create Firefox browser with proxy access to the outside.
+    BROWSER_BRAND = 'FIREFOX'
+
+    ACCEPT_SSL_CERTS = False
+    HTTP_PROXY_HOST = 'one.proxy.att.com'
+    HTTP_PROXY_PORT = 8080
+
+    def qa_open_website(self):
+        """ Firefox opens a Website.
+
+        Visit SITE_URL and prove the browser went there.
+        """
+
+        SITE_URL = 'https://www.google.com/'
+        SITE_TITLE = 'Google'  # part of title
+
+        # self.driver is the WebDriver object.
+        print("Starting browser session...")
+        self.driver.get(SITE_URL)
+
+        print('Found Page: url={} title="{}"'.format(
+            self.driver.current_url, self.driver.title))
+
+        self.assertEqual(self.driver.current_url, SITE_URL)
+        self.assertIn(SITE_TITLE, self.driver.title)
