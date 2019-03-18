129eafc8899ee97bb5096aa44829bd8e7add9e07
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2019-03-18 15:44:04 -0400

    [MIC-000] Refactored the qa-sutie modules into single copies with the HEADLESS = True/False flag.

--- qa-suite/qa_chrome_example.py
+++ qa-suite/qa_chrome_example.py
@@ -1,28 +1,39 @@
-""" QA Testing: SeleniumWebGUITestCase Class.
+""" QA Testing: Chrome Example.
 
-The SeleniumWebGUITestCase is a helper class for QA Testing.
-We are testing it here with itself, which is confusing.
+Headless Mode: HEADLESS = True (default)
 
-Selenium Grid opens a remote browser and traverses the UI.
-There is no visible GUI shown during these QA tests.
+    Selenium Grid opens a remote browser and traverses the UI.
+    There is no visible GUI shown during these QA tests.
+
+Visible Mode: HEADLESS = False
+
+    Selenium Non-Headless opens a local browser and traverses the UI.
+    There must be a locally installed Web browser & WebDriver.
+    A visible GUI shows the events during these QA tests.
 
 The example below demonstrates a Chrome browser session.
+For "Visible Mode", these programs must be installed.
+    Web Browser: google-chrome-stable
+    WebDriver:   chromedriver
 """
 
 from testharness.selenium.testcases import SeleniumWebGUITestCase
 
+from time import sleep
+
 
 class QAChromeWebGUI(SeleniumWebGUITestCase):
-    """ QA FireFox Web GUI.
+    """ QA Chrome Web GUI.
 
     Prove that QA tests run with the Chrome Web GUI settings.
-    This sets up a remote Chrome Web Driver in a Selenium Grid.
+    This sets up a visible Firefox Web Driver in a Selenium Local session.
     """
 
     GRID_URL = 'http://roattnap03.gcsc.att.com:4444/wd/hub'
 
     # Settings: Create Chrome browser with proxy access to the outside.
     BROWSER_BRAND = 'Chrome'
+    HEADLESS = True
 
     ACCEPT_SSL_CERTS = False
     HTTP_PROXY_HOST = 'one.proxy.att.com'
@@ -39,6 +50,7 @@ class QAChromeWebGUI(SeleniumWebGUITestCase):
 
         # self.driver is the WebDriver object.
         self.driver.get(SITE_URL)
+        sleep(3)
 
         self.assertEqual(self.driver.current_url, SITE_URL,
                          'Opened Web page: {}'.format(SITE_URL))
--- qa-suite/qa_firefox_example.py
+++ qa-suite/qa_firefox_example.py
@@ -1,31 +1,39 @@
-""" QA Testing: SeleniumWebGUITestCase Class.
+""" QA Testing: Firefox Example.
 
-The SeleniumWebGUITestCase is a helper class for QA Testing.
-We are testing it here with itself, which is confusing.
+Headless Mode: HEADLESS = True (default)
 
-Selenium Grid opens a remote browser and traverses the UI.
-There is no visible GUI shown during these QA tests.
+    Selenium Grid opens a remote browser and traverses the UI.
+    There is no visible GUI shown during these QA tests.
+
+Visible Mode: HEADLESS = False
+
+    Selenium Non-Headless opens a local browser and traverses the UI.
+    There must be a locally installed Web browser & WebDriver.
+    A visible GUI shows the events during these QA tests.
 
 The example below demonstrates a Firefox browser session.
+For "Visible Mode", these programs must be installed.
+    Web Browser: firefox
+    WebDriver:   geckodriver
 """
 
 from testharness.selenium.testcases import SeleniumWebGUITestCase
 
+from time import sleep
+
 
 class QAFirefoxWebGUI(SeleniumWebGUITestCase):
-    """ QA FireFox Web GUI.
+    """ QA Firefox Web GUI.
 
     Prove that QA tests run with the Firefox Web GUI settings.
-    This sets up a remote Firefox Web Driver in a Selenium Grid.
+    This sets up a visible Firefox Web Driver in a Selenium Local session.
     """
 
     GRID_URL = 'http://roattnap03.gcsc.att.com:4444/wd/hub'
 
     # Settings: Create Firefox browser with proxy access to the outside.
-    BROWSER_BRAND = 'FireFox'
-    VERSION = None
-
-    PAGE_LOAD_TIMEOUT = 2.5  # seconds
+    BROWSER_BRAND = 'Firefox'
+    HEADLESS = True
 
     ACCEPT_SSL_CERTS = False
     HTTP_PROXY_HOST = 'one.proxy.att.com'
@@ -37,11 +45,12 @@ class QAFirefoxWebGUI(SeleniumWebGUITestCase):
         Visit SITE_URL and prove the browser went there.
         """
 
-        SITE_URL = 'https://www.google.com/'
-        SITE_TITLE = 'Google'  # part of title
+        SITE_URL = 'https://www.python.org/'
+        SITE_TITLE = 'Python'  # part of title
 
         # self.driver is the WebDriver object.
         self.driver.get(SITE_URL)
+        sleep(3)
 
         self.assertEqual(self.driver.current_url, SITE_URL,
                          'Opened Web page: {}'.format(SITE_URL))
